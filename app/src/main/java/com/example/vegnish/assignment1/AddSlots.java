5ae375187663fec694d56c6c66767313ae96335c
unknown <anjana_96hotmail.com>
2018-05-06 23:42:12 +0800

    complete Add slots mock up

--- app/src/main/java/com/example/vegnish/assignment1/AddSlots.java
+++ app/src/main/java/com/example/vegnish/assignment1/AddSlots.java
@@ -2,12 +2,41 @@ package com.example.vegnish.assignment1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddSlots extends AppCompatActivity {
+    private Spinner spinner;
+    private static final String[]paths = {"Chief Invigilator", "Invigilator", "Standby Invigilator"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_slots);
+        setTitle("Add Slots");
+        spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(AddSlots.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        //spinner.setOnItemSelectedListener(this);
+
     }
+
+//    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+//
+//        switch (position) {
+//            case 0:
+//                // Whatever you want to happen when the first item gets selected
+//                break;
+//            case 1:
+//                // Whatever you want to happen when the second item gets selected
+//                break;
+//            case 2:
+//                // Whatever you want to happen when the thrid item gets selected
+//                break;
+//
+//        }
+//    }
 }
