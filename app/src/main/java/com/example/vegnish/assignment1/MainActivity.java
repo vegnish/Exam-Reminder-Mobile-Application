b89dcec773fa0e4db70afd3f5143736f11532d05
Chamode <anjana_96hotmail.com>
2018-05-20 01:42:56 +0800

    working navDrawer

--- app/src/main/java/com/example/vegnish/assignment1/MainActivity.java
+++ app/src/main/java/com/example/vegnish/assignment1/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
-                Intent homeIntent = new Intent(MainActivity.this, AddSlots.class);
+                Intent homeIntent = new Intent(MainActivity.this, Home.class);
                 startActivity(homeIntent);
                 finish();
             }
