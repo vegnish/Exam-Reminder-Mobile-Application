a3b161ee976b1d691fe7e6f2601ec76a6d399eba
Vegnish <vegnishrao93@msn.com>
2018-05-20 11:34:53 +0800

    Added Announcements Page (working). Included numerous audio and image files

--- app/src/main/java/com/example/vegnish/assignment1/MainActivity.java
+++ app/src/main/java/com/example/vegnish/assignment1/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.vegnish.assignment1;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 4500;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
