52e55cbd7508fd48ad77d84b8fb5e7ecc1f5a835
Vegnish <vegnishrao93@msn.com>
2018-05-21 05:36:33 +0800

    Added a working Welcome Page. Few other UI changes in MainActivity

--- app/src/main/java/com/example/vegnish/assignment1/MainActivity.java
+++ app/src/main/java/com/example/vegnish/assignment1/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
-                Intent homeIntent = new Intent(MainActivity.this, Home.class);
+                Intent homeIntent = new Intent(MainActivity.this, Welcome.class);
                 startActivity(homeIntent);
                 finish();
             }
